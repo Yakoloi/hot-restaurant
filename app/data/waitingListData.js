307de915e95fc6cae76621d4bc3afcb8bc88be9b
Dennis <dennisalvey423@gmail.com>
2017-09-13 19:21:39 -0700

    waiting list

---
+++ app/data/waitingListData.js
@@ -0,0 +1,11 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+var path = require("path");
+
+var waitingList = [];
+
+var tableData = [{
+    name: nameInput,
+    phone: phoneInput,
+    id: idInput
+  }];
