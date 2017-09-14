c869510bb15cc549da7b335842a816d650ae20a4
Yakoloi <yakoloi@gmail.com>
2017-09-13 19:09:20 -0700

    objects

---
+++ app/data/tableData.js
@@ -0,0 +1,12 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+var path = require("path");
+
+var reserved = [];
+
+var tableData = [{
+    name: nameInput,
+    phone: phoneInput,
+    email: emailInput,
+    id: idInput
+  }];
